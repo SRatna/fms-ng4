56a82819d49794e1b9bcc0120ee7b5f845b11e51
SRatna <adhikariratna52@gmail.com>
2017-05-17 13:49:30 +0545

    started working on departments

---
+++ src/app/modules/departments/departments-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DepartmentComponent } from './components/department/department.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DepartmentComponent]
+})
+export class DepartmentsModule { }
