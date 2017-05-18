1251318951120ba1c9994d7e142112f34b121070
SRatna <adhikariratna52@gmail.com>
2017-05-18 16:00:59 +0545

    modes module scaffolded

---
+++ src/app/modules/modes/components/modes/modes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modes',
+  templateUrl: './modes.component.html',
+  styleUrls: ['./modes.component.css']
+})
+export class ModesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/modes/modes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModesComponent } from './components/modes/modes.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ModesComponent]
+})
+export class ModesModule { }
