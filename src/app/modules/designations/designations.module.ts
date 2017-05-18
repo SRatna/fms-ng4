158fdc3496d69b7f6b4a6070d01e7385210baf34
SRatna <adhikariratna52@gmail.com>
2017-05-18 14:44:23 +0545

    designations module scaffolded

---
+++ src/app/modules/designations/designations.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DesignationsComponent } from './components/designations/designations.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DesignationsComponent]
+})
+export class DesignationsModule { }
