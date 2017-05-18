c16dd4378cb05528eab80fc6f1b8006abb0f5ccd
SRatna <adhikariratna52@gmail.com>
2017-05-18 16:37:47 +0545

    types module scaffolded

---
+++ src/app/modules/types/types.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TypesComponent } from './components/types/types.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TypesComponent]
+})
+export class TypesModule { }
