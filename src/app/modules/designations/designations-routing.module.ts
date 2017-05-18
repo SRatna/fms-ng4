4f1362d7c39908dac676346741122bd53fee8031
SRatna <adhikariratna52@gmail.com>
2017-05-18 15:17:37 +0545

    styled sidenav

--- src/app/modules/designations/designations-routing.module.ts
+++ src/app/modules/designations/designations-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DesignationsComponent } from './components/designations/designations.component'
+import { DesignationsComponent } from './components/designations/designations.component';
 
 const appRoutes: Routes = [
-  { path: 'categories', component: DesignationsComponent },
+  { path: 'designations', component: DesignationsComponent },
 ];
 @NgModule({
   imports: [
