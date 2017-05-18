1e865ed9aa885b65299acae3c7694eed06c84ccd
SRatna <adhikariratna52@gmail.com>
2017-05-18 15:20:21 +0545

    grades scaffolded

---
+++ src/app/modules/grades/grades.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GradesComponent } from './components/grades/grades.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GradesComponent]
+})
+export class GradesModule { }
