8f43bcfde96a68039691ad3772b2dbaad4658cce
SRatna <adhikariratna52@gmail.com>
2017-05-18 16:53:33 +0545

    projects module scaffolded

---
+++ src/app/modules/projects/projects.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProjectsComponent]
+})
+export class ProjectsModule { }
