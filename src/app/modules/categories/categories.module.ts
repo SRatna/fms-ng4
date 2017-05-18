618f870e7bcb946538105497ae23525fb41a80ff
SRatna <adhikariratna52@gmail.com>
2017-05-18 13:49:09 +0545

    categories scaffolded

---
+++ src/app/modules/categories/categories.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesComponent } from './components/categories/categories.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CategoriesComponent]
+})
+export class CategoriesModule { }
