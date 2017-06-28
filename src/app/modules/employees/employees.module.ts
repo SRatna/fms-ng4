3477b66cc2f35a6dabd5b3aec8b30b6ca2acaaab
johnsunam <sunamjohn@gmail.com>
2017-06-28 15:16:17 +0545

    issue resolved

--- src/app/modules/employees/employees.module.ts
+++ src/app/modules/employees/employees.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { NewEmployeeComponent } from './components/new-employee/new-employee.component';
-import { EmployeesRoutingModule } from './employees-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NewEmployeeComponent} from './components/new-employee/new-employee.component';
+import {EmployeesRoutingModule} from './employees-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-import { CustomMaterialModule } from '../../custom-material.module';
-import { EmployeesComponent } from './components/employees/employees.component';
-import { EmployeeAttendence } from './components/employee-attendence/employee-attendence.component';
+import {CustomMaterialModule} from '../../custom-material.module';
+import {EmployeesComponent} from './components/employees/employees.component';
+import {EmployeeAttendence} from './components/employee-attendence/employee-attendence.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -16,8 +16,9 @@ import { EmployeeAttendence } from './components/employee-attendence/employee-at
     BrowserAnimationsModule,
     CustomMaterialModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   declarations: [NewEmployeeComponent, EmployeesComponent, EmployeeAttendence]
 })
-export class EmployeesModule { }
+export class EmployeesModule {}
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,19 +1,33 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
 @Injectable()
 export class DataService {
-    constructor(private http: Http) { }
+    constructor(private http : Http) {}
 
-    getData(url: string, data: any) {
-        return this.http.post(url, data);
+    getData(url : string, data : any) {
+        return this
+            .http
+            .post(url, data);
     }
-    getDatas(url: string) {
-       return this.http.get(url);
+    getDatas(url : string) {
+        return this
+            .http
+            .get(url);
     }
-    saveData(url: string, data: object) {
-        return this.http.post(url, data);
+    saveData(url : string, data : object) {
+        return this
+            .http
+            .post(url, data);
     }
-    getDataByDateRange(url:string,data:any) {
-        return this.http.post(url,data);
+    getDataByDateRange(url : string, data : any) {
+        return this
+            .http
+            .post(url, data);
+    }
+    getDataById(url : string, id : any) {
+        let fullUrl = url + '/' + id;
+        return this
+            .http
+            .get(fullUrl)
     }
 }
